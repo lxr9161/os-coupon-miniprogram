0f00f91f00e8700b44acb3c0a29ce87baf468708
liuxiaorong <liuxiaorong@taqu.cn>
2022-03-20 16:52:41 +0800

    配置文件

---
+++ config.js
@@ -0,0 +1,14 @@
+//接口请求域名
+export let apiHost = ''
+//图片域名
+export let imgHost = ''
+//消息模版
+export let templates = [
+	//优惠券提醒模板id
+	'',
+	//收益到账提醒模板id
+	''
+]
+
+//七牛空间上传域名，根据空间所在区域不通选择对应的上传域名，具体查看七牛文档
+export let uploadQiniuHost = ''
